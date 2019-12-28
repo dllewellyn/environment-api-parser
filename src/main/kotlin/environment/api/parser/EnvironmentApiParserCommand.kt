a32ef21336c794a854da8a270dce761472a5ea00
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2019-12-28 10:50:30 +0000

    Initial commit

---
+++ src/main/kotlin/environment/api/parser/EnvironmentApiParserCommand.kt
@@ -0,0 +1,30 @@
+package environment.api.parser
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "environment-api-parser", description = ["..."],
+        mixinStandardHelpOptions = true)
+class EnvironmentApiParserCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(EnvironmentApiParserCommand::class.java, *args)
+        }
+    }
+}
---
+++ src/test/java/environment/api/parser/EnvironmentApiParserCommandTest.java
@@ -0,0 +1,28 @@
+package environment.api.parser;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EnvironmentApiParserCommandTest {
+
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[] { "-v" };
+            PicocliRunner.run(EnvironmentApiParserCommand.class, ctx, args);
+
+            // environment-api-parser
+            assertTrue(baos.toString().contains("Hi!"));
+        }
+    }
+}
